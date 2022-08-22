7e063d07f89bd8bb13658edc732959e4a67b36b6
Javier Alejandro Azurdia Arrecis <aleazurdia1@gmail.com>
2022-08-21 22:22:17 -0600

    Final. Quedó bonito.

--- app/src/main/java/com/example/lab6/MainActivity.kt
+++ app/src/main/java/com/example/lab6/MainActivity.kt
@@ -1,23 +1,14 @@
 package com.example.lab6
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.FragmentContainerView
 import androidx.fragment.app.commit
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var homebtn: Button
-    private lateinit var searchbtn: Button
-    private lateinit var playlistbtn: Button
     private lateinit var bottomNav: BottomNavigationView
-
-
-
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -35,7 +26,7 @@ class MainActivity : AppCompatActivity() {
             when(it.itemId){
                 R.id.homebtn -> setFragment(firstFragment())
                 R.id.searchbtn -> setFragment(secondFragment())
-
+                R.id.playlistbtn -> setFragment(thirdFragment())
 
             }
         true
--- app/src/main/java/com/example/lab6/thirdFragment.kt
+++ app/src/main/java/com/example/lab6/thirdFragment.kt
@@ -1,15 +1,14 @@
 package com.example.lab6
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
+import android.widget.ImageButton
+import android.widget.TextView
+import androidx.fragment.app.Fragment
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
+
 
 /**
  * A simple [Fragment] subclass.
@@ -17,5 +16,27 @@ private const val ARG_PARAM2 = "param2"
  * create an instance of this fragment.
  */
 class thirdFragment : Fragment(R.layout.fragment_third) {
+
+    private lateinit var addBtn: ImageButton
+    private lateinit var dwSongs: TextView
+
+    private var contador: Int = 0
+
+    override fun onViewCreated(view: View, savedInstanceState:Bundle?){
+        addBtn = view.findViewById(R.id.add_btn)
+        dwSongs = view.findViewById(R.id.dw_songs)
+        setListeners()
+
+        super.onViewCreated(view, savedInstanceState)
+
+    }
+
+    private fun setListeners(){
+        addBtn.setOnClickListener {
+            contador++
+            dwSongs.text = contador.toString() + " songs"
+        }
+    }
+
 
 }
