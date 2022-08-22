a1961fa810909ac798044ec135521d61f3205f6c
Javier Alejandro Azurdia Arrecis <aleazurdia1@gmail.com>
2022-08-21 20:44:21 -0600

    Ahí vamos jaja pero la verdad jajant

--- app/src/main/java/com/example/lab6/MainActivity.kt
+++ app/src/main/java/com/example/lab6/MainActivity.kt
@@ -2,10 +2,56 @@ package com.example.lab6
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentContainerView
+import androidx.fragment.app.commit
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var homebtn: Button
+    private lateinit var searchbtn: Button
+    private lateinit var playlistbtn: Button
+    private lateinit var bottomNav: BottomNavigationView
+
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        bottomNav = findViewById(R.id.bottomNavigationView)
+        setFragment(firstFragment())
+        setListener()
+
+
     }
+
+
+    private fun setListener(){
+        bottomNav.setOnItemSelectedListener {
+            when(it.itemId){
+                R.id.homebtn -> setFragment(firstFragment())
+                //R.id.searchbtn -> setFragment(firstFragment())
+
+            }
+        true
+        }
+    }
+
+
+
+    private fun setFragment(fragment: Fragment){
+        supportFragmentManager.commit {
+            setReorderingAllowed(true)
+            replace(R.id.fragmentContainerView, fragment)
+
+        }
+    }
+
+
+
+
 }
